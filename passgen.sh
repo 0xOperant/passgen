d78dd5bbaaf84414829d61946c1eafb1a889a872
redbeard <david.bell4@ge.com>
2020-01-10 10:42:04 -0500

    initial commit

---
+++ passgen.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+charset='[:alnum:]'
+
+# set default pasword length to 10 if not specified
+length=$1
+
+if [[ -n $length ]]
+then
+    count=$length
+else
+    count=10
+fi
+
+# do we want to include symbols?
+read -q "symbols?Include symbols? (y|n) "
+
+if [[ $symbols = "y" || $symbols = "Y" ]]
+then
+    charset='[:alnum:][:punct:]'
+fi
+
+# generate 10 random passwords with the length and charset chosen
+echo "\nGenerating 10 random $count character passwords using $charset: \n"
+i=0
+while (( i++ < 10 )) { 
+    password="$(cat /dev/urandom | LC_ALL=C tr -dc $charset | head -c$count)"
+    echo $password
+    }
+print "\n"
